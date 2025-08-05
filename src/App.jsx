22c2c76e9ee3e69be34eec611c755d1f5269abb9
K-cook517 <only05250517@gmail.com>
2025-08-05 17:10:56 +0900

    자체 회원가입 및 로그인 작업 진행 중(CSS는 임시)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,70 @@
+// import './styles/common.css'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
+
+import { checkAuthStatusThunk } from './features/authSlice'
+
+// import Home from './pages/Home'
+// import Navbar from './components/shared/Navbar'
+// import Footer from './components/shared/Footer'
+
+import RegisterPage from './pages/RegisterPage'
+import LoginPage from './pages/LoginPage'
+// import ItemCreatePage from './pages/ItemCreatePage'
+
+// import ItemListPage from './pages/ItemListPage'
+// import ItemEditPage from './pages/ItemEditPage'
+// import ItemSellDetailPage from './pages/ItemSellDetailPage'
+// import MyOrderListPage from './pages/MyOrderListPage'
+// import TokenPage from './pages/TokenPage'
+// import ChatPage from './pages/ChatPage'
+// import ChartPage from './pages/ChartPage'
+
 function App() {
-   return <></>
+   const dispatch = useDispatch()
+   const location = useLocation()
+   const { isAuthenticated, user } = useSelector((state) => state.auth)
+
+   useEffect(() => {
+      dispatch(checkAuthStatusThunk())
+   }, [dispatch])
+
+   return (
+      <>
+         {/* <Navbar isAuthenticated={isAuthenticated} user={user} /> */}
+         <Routes>
+            {/* <Route path="/" element={<Home />} /> */}
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/login" element={<LoginPage />} />
+
+            {/* 상품리스트 */}
+            {/* <Route path="/items/createlist" element={<ItemListPage key={location.key} />} /> */}
+
+            {/* 상품등록 */}
+            {/* <Route path="/items/create" element={<ItemCreatePage />} /> */}
+
+            {/* 상품수정 */}
+            {/* <Route path="/items/edit/:id" element={<ItemEditPage />} /> */}
+
+            {/* 상품 상세페이지 */}
+            {/* <Route path="/items/detail/:id" element={<ItemSellDetailPage />} /> */}
+
+            {/* 주문내역 페이지 */}
+            {/* <Route path="/myorderlist" element={<MyOrderListPage />} /> */}
+
+            {/* 토큰 발급 페이지 */}
+            {/* <Route path="/token" element={<TokenPage />} /> */}
+
+            {/* 채팅 페이지 */}
+            {/* <Route path="/chat" element={<ChatPage />} /> */}
+
+            {/* 차트 페이지 */}
+            {/* <Route path="/chart" element={<ChartPage />} /> */}
+         </Routes>
+         {/* <Footer /> */}
+      </>
+   )
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import store from '../src/store/store'
+
+import '../src/styles/register.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+   //  <StrictMode>
+   <Provider store={store}>
+      <BrowserRouter>
+         <App />
+      </BrowserRouter>
+   </Provider>
+   //  </StrictMode>
 )
