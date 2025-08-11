376c1ad71163b13a3b051595373258ac7aa9d0e4
“KimTyun” <ktyy0511@gmail.com>
2025-08-11 14:56:53 +0900

    상품등록 페이지 디자인 50%

--- src/components/item/IteamCreateForm.jsx
+++ src/components/item/IteamCreateForm.jsx
@@ -1,10 +1,12 @@
 import { useDispatch } from 'react-redux'
 import { itemCreateThunk } from '../../features/itemSlice'
 import { useState } from 'react'
+import '../../styles/itemUpload.css'
 
 function IteamCreateForm() {
    const [imgUrls, setImgUrls] = useState([])
    const [imgFiles, setImgFiles] = useState([])
+   const [stock, setStock] = useState(false)
    const dispatch = useDispatch()
 
    const handleImageChange = (e) => {
@@ -38,45 +40,66 @@ function IteamCreateForm() {
    }
    return (
       <>
-         <div className="form-wrap">
+         <div className="item-upload-form">
             <form method="post" encType="multipart/form-data" onSubmit={handleSubmit}>
-               <div>
-                  <label htmlFor="name">제품명</label>
-                  <input type="text" name="" id="name" />
+               <div className="item-upload-form__imgs">
+                  <img src="#" alt="#" />
+                  <img src="#" alt="#" />
+                  <img src="#" alt="#" />
+                  <img src="#" alt="#" />
                </div>
-               <div>
-                  <label htmlFor="price">기본가격</label>
-                  <input type="text" name="" id="price" />
+               <div className="item_img">
+                  <label htmlFor="item_img">상품 이미지 등록 </label>
+                  <span> 최대 4개까지 이미지 등록이 가능합니다. (파일당 4mb 제한)</span>
+                  <input type="file" name="" id="item_img" hidden multiple accept="image/*" />
+               </div>
+               <div className="item_name_price">
+                  <div className="item_name">
+                     <label htmlFor="name">제품 이름</label>
+                     <input type="text" name="" id="name" placeholder="제품명 입력" autocomplete="off" />
+                  </div>
+                  <div className="item_price">
+                     <label htmlFor="price">기본가격</label>
+                     <input type="number" name="" id="price" placeholder="000원 (숫자만 입력)" autocomplete="off" />
+                  </div>
                </div>
                <div>
                   <label htmlFor="stock_number">재고수량</label>
-                  <input type="text" name="" id="stock_number" />
+                  <input type="number" name="" id="stock_number" autocomplete="off" placeholder="재고수량 입력" readOnly={stock} />
                </div>
-               <div>
+               <div className="item-stock">
                   <label htmlFor="status">판매상태</label>
-                  <input type="text" name="" id="status" />
+                  <div className="radio-wrap">
+                     <input type="radio" name="status" />
+                  </div>
+                  <div className="radio-wrap">
+                     <input type="radio" name="status" />
+                  </div>
+                  <div className="radio-wrap">
+                     <input type="radio" name="status" />
+                  </div>
                </div>
                <div>
                   <label htmlFor="is_sale">할인여부</label>
-                  <input type="text" name="" id="is_sale" />
+                  <input type="text" name="" id="is_sale" autocomplete="off" />
                </div>
                <div>
                   <label htmlFor="sale">할인율</label>
-                  <input type="text" name="" id="sale" />
+                  <input type="text" name="" id="sale" autocomplete="off" />
                </div>
                <div>
                   <label htmlFor="option_name">옵션 이름</label>
-                  <input type="text" name="" id="option_name" />
+                  <input type="text" name="" id="option_name" autocomplete="off" />
                </div>
                <div>
                   <label htmlFor="option_price">옵션 추가가격</label>
-                  <input type="text" name="" id="option_price" />
+                  <input type="text" name="" id="option_price" autocomplete="off" />
                </div>
                <div>
                   <label htmlFor="option_req_item_yn">옵션 대표상품 여부</label>
-                  <input type="text" name="" id="option_req_item_yn" />
+                  <input type="text" name="" id="option_req_item_yn" autocomplete="off" />
                </div>
-               <input type="file" name="" id="img" multiple accept="image/*" />
+               <input type="file" name="" id="img" />
                <button type="submit">등록하기</button>
             </form>
          </div>
